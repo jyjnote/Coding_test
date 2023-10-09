74fdef7196f7f50b6f0468b8de91c4148f3c8af9
Jaeyeon <144209498+jyjnote@users.noreply.github.com>
2023-10-09 16:44:43 +0900

    [Bronze II] Title: 음계, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,16 +1,8 @@
-def determine_order(notes):
-    ascending_order = [1, 2, 3, 4, 5, 6, 7, 8]
-    descending_order = [8, 7, 6, 5, 4, 3, 2, 1]
+seq = list(map(int, input().split()))
 
-    if notes == ascending_order:
-        return "ascending"
-    elif notes == descending_order:
-        return "descending"
-    else:
-        return "mixed"
-
-
-notes = list(map(int, input().split()))
-
-result = determine_order(notes)
-print(result)
+if seq == [1, 2, 3, 4, 5, 6, 7, 8]:
+    print('ascending')
+elif seq == [8, 7, 6, 5, 4, 3, 2, 1]:
+    print('descending')
+else:
+    print('mixed')

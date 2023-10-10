e32b292c4adc95566eac5987091cbda2e4c6243c
Jaeyeon <144209498+jyjnote@users.noreply.github.com>
2023-10-10 14:17:52 +0900

    [Silver V] Title: 나이순 정렬, Time: 376 ms, Memory: 134660 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,21 +1,11 @@
-n = int(input())  # 회원 수 입력
+n = int(input())
 
-members_dict = {}  # 나이를 키로 가지는 딕셔너리
-
-# 회원 정보 입력받기
+m = []
 for _ in range(n):
-    age, name = map(str, input().split())
-    age = int(age)
+    a, b = input().split()
+    m.append((int(a), b))
 
-    if age not in members_dict:
-        members_dict[age] = []
+m.sort(key=lambda x: (x[0]))
 
-    members_dict[age].append(name)
-
-# 나이를 기준으로 오름차순 정렬
-sorted_ages = sorted(members_dict.keys())
-
-# 정렬된 나이에 대해 회원 이름 출력
-for age in sorted_ages:
-    for name in members_dict[age]:
-        print(age, name)
+for a in m:
+    print(a[0],a[1])

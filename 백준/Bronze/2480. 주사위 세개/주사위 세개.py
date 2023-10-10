e2fd7385a47a2c3f4a324da97d7bda7fb5bcf76f
Jaeyeon <144209498+jyjnote@users.noreply.github.com>
2023-10-10 11:10:17 +0900

    [Bronze IV] Title: 주사위 세개, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,19 +1,12 @@
-def calculate_prize(dice):
-    # 주어진 주사위 눈의 빈도수 계산
-    dice_counts = [dice.count(i) for i in range(1, 7)]
-    
-    if max(dice_counts) == 3:
-        return 10000 + (dice_counts.index(3) + 1) * 1000
-    elif max(dice_counts) == 2:
-        return 1000 + (dice_counts.index(2) + 1) * 100
-    else:
-        return max(dice) * 100
-
-# 3개 주사위의 눈 입력 받기
 dice = list(map(int, input().split()))
 
-# 상금 계산
-prize = calculate_prize(dice)
+counts = [dice.count(i) for i in range(1, 7)]
 
-# 결과 출력
+if 3 in counts:
+    prize = 10000 + (counts.index(3) + 1) * 1000
+elif 2 in counts:
+    prize = 1000 + (counts.index(2) + 1) * 100
+else:
+    prize = max(dice) * 100
+
 print(prize)

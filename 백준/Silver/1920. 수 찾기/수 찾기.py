c585ab7666336ae4b7518ea9011fbd5d3fd20d4e
Jaeyeon <144209498+jyjnote@users.noreply.github.com>
2023-10-10 17:08:39 +0900

    [Silver IV] Title: 수 찾기, Time: 208 ms, Memory: 144144 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,11 @@
+N = int(input()) 
+n = set(map(int, input().split()))  
+
+M = int(input()) 
+m = list(map(int, input().split()))  
+
+for i in m:   
+    if i in n:
+        print('1')
+    else:
+        print('0')
